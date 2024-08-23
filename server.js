dab853428f2bbf817969bc74ff98f83fdea2bc53
Diego Enrique Manzanarez Velazquez <A01653042@tec.mx>
2024-08-22 21:26:32 -0600

    Added latest checksum retrieval

--- server.js
+++ server.js
@@ -8,37 +8,42 @@ mongoose.connect('mongodb+srv://diego:madmach123@cluster0.bq7iukp.mongodb.net/MA
 const app = express();
 app.use(bodyParser.json());
 
-
 // Define a schema and model for the checksum, specifying the collection name
 const checksumSchema = new mongoose.Schema({
     documentId: String,
     checksum: String,
     timestamp: { type: Date, default: Date.now }
-  }, { collection: 'MD5Checksums' });
-  
-  const MD5Checksums = mongoose.model('MD5Checksums', checksumSchema);
-  
-  // Endpoint to receive checksum data
-  app.post('/saveChecksum', async (req, res) => {
+}, { collection: 'MD5Checksums' });
+
+const MD5Checksums = mongoose.model('MD5Checksums', checksumSchema);
+
+app.post('/saveChecksum', async (req, res) => {
     try {
-      const { documentId, checksum } = req.body;
-  
-      const newChecksum = new MD5Checksums({ documentId, checksum });
-      await newChecksum.save();
-      res.status(200).send('Checksum saved successfully');
+        const { documentId, checksum } = req.body;
+
+        const latestChecksum = await MD5Checksums.findOne({ documentId }).sort({ timestamp: -1 });
+
+        if (latestChecksum) {
+            if (latestChecksum.checksum === checksum) {
+                return res.status(200).send('Checksum is identical to the last one, no need to save.');
+            }
+        }
+
+        const newChecksum = new MD5Checksums({ documentId, checksum });
+        await newChecksum.save();
+        res.status(200).send('Checksum saved successfully');
     } catch (err) {
-      res.status(500).send('Error saving checksum');
+        console.error('Error saving checksum:', err);
+        res.status(500).send('Error saving checksum');
     }
-  });
+});
 
 app.get('/hi', (req, res) => {
-    console.log("Success")
-    res.status(200).send('Checksum saved successfully');
-  });
-  
-  // Start the server
-  const PORT = process.env.PORT || 5050;
-  app.listen(PORT, () => {
+    console.log("Success");
+    res.status(200).send('Hello!');
+});
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-  });
-  
+});

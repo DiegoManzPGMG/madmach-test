643f86a8917a4db110a44b81aeeb1cc1dba30d53
Diego Enrique Manzanarez Velazquez <A01653042@tec.mx>
2024-08-29 17:40:37 -0600

    Changed documentId to folderId

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://diego:madmach123@cluster0.bq7iukp.mongodb.net/MADMACH-TEST', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 
 // Define a schema and model for the checksum, specifying the collection name
 const checksumSchema = new mongoose.Schema({
-    documentId: String,
+    folderId: String,
     checksum: String,
     timestamp: { type: Date, default: Date.now }
 }, { collection: 'MD5Checksums' });
@@ -19,24 +20,34 @@ const MD5Checksums = mongoose.model('MD5Checksums', checksumSchema);
 
 app.post('/saveChecksum', async (req, res) => {
     try {
-        const { documentId, checksum } = req.body;
+        const { folderId, checksum } = req.body;
 
-        const latestChecksum = await MD5Checksums.findOne({ documentId }).sort({ timestamp: -1 });
+        const latestChecksum = await MD5Checksums.findOne({ folderId }).sort({ timestamp: -1 });
 
-        if (latestChecksum) {
-            if (latestChecksum.checksum === checksum) {
-                return res.status(200).send('Checksum is identical to the last one, no need to save.');
-            }
+        if (latestChecksum && latestChecksum.checksum === checksum) {
+            return res.status(200).send('Checksum is identical to the last one, no need to save nor vectorize.');
         }
 
-        const newChecksum = new MD5Checksums({ documentId, checksum });
-        await newChecksum.save();
-        res.status(200).send('Checksum saved successfully');
+        try {
+            const response = await axios.get('http://3.219.90.64:3030/run-script', {
+                documentId,
+                checksum
+            });
+            console.log('External API response:', response.data);
+            const newChecksum = new MD5Checksums({ folderId, checksum });
+            await newChecksum.save();
+
+            return res.status(200).send('Checksum saved and vectorization executed successfully.');
+        } catch (apiError) {
+            console.error('Error calling external API:', apiError);
+            return res.status(500).send('Error calling external API');
+        } 
     } catch (err) {
         console.error('Error saving checksum:', err);
         res.status(500).send('Error saving checksum');
     }
 });
+
 
 app.get('/hi', (req, res) => {
     console.log("Success");
